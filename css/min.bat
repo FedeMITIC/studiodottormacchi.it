cb92aa0c0cfff8e8516d79777e1b414f5094c7ee
Federico Macchi <federico.macchi@mail.polimi.it>
2018-03-05 22:20:27 +0100

    Minor fixes to min.bat files

--- css/min.bat
+++ css/min.bat
@@ -1,7 +1,9 @@
 @echo off
 REM Delete main.min.css
 IF EXIST main.min.css DEL /F main.min.css
+IF EXIST custom.min.css DEL /F custom.min.css
 REM Re create main.min.css w/ source code updated
 uglifycss buttons.css modals.css default.css --output main.min.css
+uglifycss custom.css --output custom.min.css
 REM Pause to see output
 PAUSE > nul
--- js/min.bat
+++ js/min.bat
@@ -2,7 +2,9 @@
 REM Delete main.min.js
 IF EXIST main.min.js DEL /F main.min.js
 REM Re create main.min.js w/ source code updated
-REM css-filters-polyfills must be excluded from bundle because must be instantiated before being referenced
+REM css-filters-polyfill.js must be excluded from bundle because must be instantiated before being referenced
 uglifyjs classie.js custom.js modalEffects.js modernizr.custom.js cssParser.js -o main.min.js
+REM minify css-filters-polyfill only
+uglifyjs css-filters-polyfill.js -o css-filters-polyfill.min.js
 REM Pause to see output
 PAUSE > nul
