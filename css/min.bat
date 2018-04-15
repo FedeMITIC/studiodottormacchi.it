12b454671b49024231e27fafd5df550f87302e1c
Federico Macchi <federico.macchi@mail.polimi.it>
2018-04-15 21:05:09 +0200

    updates

--- css/min.bat
+++ css/min.bat
@@ -1,8 +1,5 @@
 @echo off
-REM Delete main.min.css
-IF EXIST main.min.css DEL /F main.min.css
-IF EXIST custom.min.css DEL /F custom.min.css
-REM Re create main.min.css w/ source code updated
+REM Create main.min.css w/ source code updated
 uglifycss buttons.css modals.css default.css --output main.min.css
 uglifycss custom.css --output custom.min.css
 REM Pause to see output
